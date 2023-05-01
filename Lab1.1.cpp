3914e0b00d03cfa03d2ee4f92865714174074dae
MikheievBoghdan <132290946+MikheievBoghdan@users.noreply.github.com>
2023-05-01 22:39:45 +0300

    LAB1 КЛАСИ І ОБ'ЄКТИ В С++
    Варіант 13. Створити клас «комплексне число», такий, що:
    a) його екземпляр містить дійсну і уявну частину - змінні з плаваючою
    точкою.
    b) його конструктор без параметра створює екземпляр зі значенням 0.0
    + i0.0, а конструктор з параметрами створює екземпляр з
    відповідною дійсною та уявною частиною.
    c) його методи дозволяють отримувати і присвоювати значення дійсної
    та уявної частини.
    d) функція print () виводить на екран значення екземпляра у вигляді
    (0.00 ± i0.00).

---
+++ Lab1.1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+
+class ComplexNumber {
+private: //модифікатор доступу
+    double realPart;
+    double imaginaryPart;
+
+public: //модифікатор доступу
+    ComplexNumber() : realPart(0.0), imaginaryPart(0.0) {
+        std::cout << "Default constructor called" << std::endl;
+    }
+
+    ComplexNumber(double real, double imaginary) : realPart(real), imaginaryPart(imaginary) {
+        std::cout << "Parameterized constructor called" << std::endl;
+    }
+
+    ~ComplexNumber() {
+        std::cout << "Destructor called" << std::endl;
+    }
+
+    double getRealPart() {
+        return realPart;
+    }
+
+    void setRealPart(double real) {
+        realPart = real;
+    }
+
+    double getImaginaryPart() {
+        return imaginaryPart;
+    }
+
+    void setImaginaryPart(double imaginary) {
+        imaginaryPart = imaginary;
+    }
+
+    void print() {
+        std::cout << "(" << realPart << " " << (imaginaryPart >= 0 ? "+" : "-") << " i" << fabs(imaginaryPart) << ")" << std::endl;
+    }
+};
+
+
+
+int main() {
+    ComplexNumber number1; // виклик конструктора без параметрів
+    number1.print(); // вивід: (0.00 + i0.00)
+
+    ComplexNumber number2(2.5, -3.2); // виклик конструктора з параметрами
+    number2.print(); // вивід: (2.50 - i3.20)
+
+    return 0;
+}
